46e6afaa422cdcec6352820d35b025901685a397
Ding, Cindy <cindy_ding@optum.com>
2024-10-28 12:53:02 -0700

    inserted new data

---
+++ migrations/V1.3.1__add_new_data.sql
@@ -0,0 +1,10 @@
+USE SCHEMA DEMO;
+INSERT INTO HELLO_WORLD (FIRST_NAME, LAST_NAME, AGE)
+VALUES
+("Wilman", "Kala", 45),
+("Karl", "Jablonski", 32),
+("Justin", "Giles", 56),
+("Neel", "Kennedy", 23),
+("Julie", "Parsons", 7),
+("Nicholas", "Saint", 42),
+("Arthur", "Merlin", 84);
